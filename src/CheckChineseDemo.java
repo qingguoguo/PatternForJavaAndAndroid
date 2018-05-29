ae970528bf48de5dad241b797d2b39cd0e4e30ea
wangqing <350051556@qq.com>
2018-05-29 10:50:33 +0800

    update

---
+++ src/CheckChineseDemo.java
@@ -0,0 +1,13 @@
+import jdk.nashorn.internal.runtime.regexp.joni.Regex;
+
+
+public class CheckChineseDemo {
+
+
+    public static void main(String args[]) {
+
+        boolean isChinese = "A".matches("^[\\u4e00-\\u9fa5a-zA-Z]+$");
+
+        System.out.println("全为中文：" + isChinese);
+    }
+}
